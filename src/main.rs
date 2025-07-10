c77d3ebce304b1a064ebe25ec1139236208592b9
Oscar Aspelin <130501807+OscarAspelin95@users.noreply.github.com>
2025-07-10 19:19:13 +0200

    First draft

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use dioxus::prelude::*;
+
+use components::alignment::Alignment;
+
+mod components;
+mod utils;
+// We can import assets in dioxus with the `asset!` macro. This macro takes a path to an asset relative to the crate root.
+// The macro returns an `Asset` type that will display as the path to the asset in the browser or a local path in desktop bundles.
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+// The asset macro also minifies some assets like CSS and JS to make bundled smaller
+const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+
+
+        Alignment {}
+    }
+}
